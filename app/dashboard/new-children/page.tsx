01491944ab76a469eed60cddcddef8d4d474379b
jacquelin <jacquelinjantman@gmail.com>
2026-05-14 20:26:19 -0300

    fix fetch from new-child

--- app/dashboard/new-children/page.tsx
+++ app/dashboard/new-children/page.tsx
@@ -35,7 +35,7 @@ export default function NuevoHijoPage() {
       return
     }
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/new-children`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/children`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
